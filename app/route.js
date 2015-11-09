47807a119567c369d0253ac65eb40c9fac2d5aac
GSegobia <gabriel_segobia@hotmail.com>
2015-11-08 23:22:56 -0200

    crud concluido

--- app/route.js
+++ app/route.js
@@ -41,6 +41,43 @@ module.exports = function(app){
 		});
 	});
 
+	app.put('/api/users/:user_id', function(req, res){
+
+		User.findById(req.params.user_id, function(err, user){
+
+			if(err)
+				res.send(err);
+
+			if(req.body.name != null)
+				user.name = req.body.name;
+			if(req.body.username != null)
+				user.username = req.body.username;
+			if(req.body.password != null)
+				user.email = req.body.email;
+			if(req.body.password != null)
+				user.password = req.body.password;
+
+			user.save(function(err){
+
+				if(err)
+					res.send(err);
+
+				res.json({message: 'Usário Atualizado'});
+			});
+		});
+	});
+
+	app.delete('/api/users/:user_id', function(req, res){
+
+		User.remove({_id: req.params.user_id}, function(err, user){
+
+			if(err)
+				res.send(err);
+
+			res.json({message: 'Usuário deletado com sucesso!'})
+		});
+	});
+
 	app.get('*', function(req, res){
 
 		res.sendfile('./public/views/index.html');
